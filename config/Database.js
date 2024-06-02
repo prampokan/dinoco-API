9b9e572801aca87e9fa0258b054327b42cfe02b6
prampokan <prampokan@gmail.com>
2024-06-02 19:50:04 +0700

    fix bos

--- config/Database.js
+++ config/Database.js
@@ -1,10 +1,10 @@
 import { Sequelize } from "sequelize";
 
 const db = new Sequelize("cars_db", "root", "", {
-  host: DB_HOST,
-  user: DB_USERNAME,
-  password: DB_PASSWORD,
-  database: DB_DBNAME,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
   dialect: "mysql",
 });
 
