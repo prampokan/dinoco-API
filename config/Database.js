7a4baabd1b2d62c569b17fa4ac2488651b05aa52
prampokan <prampokan@gmail.com>
2024-06-02 19:58:53 +0700

    bismillah isok

--- config/Database.js
+++ config/Database.js
@@ -2,6 +2,9 @@ import { Sequelize } from "sequelize";
 
 const db = new Sequelize(process.env.DB_DBNAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
   dialect: "mysql",
 });
 
